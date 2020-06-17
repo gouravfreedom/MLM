0ddd7dc6cc67d871398bf46e24296abff236d8da
gouravfreedom <gouravfreedom@gmail.com>
2020-06-17 22:45:34 +0530

    Added additional basic setup

---
+++ MLM/Utils/ApplicationConstant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace MLM.Utils
+{
+    public class ApplicationConstant
+    {
+        public static String BaseUrl
+        {
+            get
+            {
+                return "http://3.0.82.226";
+            }
+
+
+        }
+
+        public static String Api
+        {
+            get
+            {
+                return "/api";
+            }
+
+        }
+
+        public static String Version
+        {
+            get
+            {
+                return "/v1";
+            }
+
+        }
+    }
+}
